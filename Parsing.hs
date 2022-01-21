07482e5f2f2128603cb0af06bd8574c593732734
Daniel-Rybe <71426854+Daniel-Rybe@users.noreply.github.com>
2022-01-21 16:32:55 +0300

    added ParserT transformer

--- Parsing.hs
+++ Parsing.hs
@@ -11,6 +11,7 @@ module Parsing
     ) where
 
 import           Control.Applicative
+import           Control.Monad                  ( join )
 import           Data.Char                      ( isAlpha
                                                 , isAlphaNum
                                                 , isDigit
@@ -18,96 +19,124 @@ import           Data.Char                      ( isAlpha
                                                 , isSpace
                                                 , isUpper
                                                 )
+import           Data.Functor.Identity          ( Identity(runIdentity) )
+import           Data.Maybe                     ( fromJust
+                                                , isJust
+                                                )
 
--- Basic definitions
+newtype ParserT m a = P (String -> m [(a, String)])
 
-newtype Parser a = P (String -> [(a, String)])
+type Parser = ParserT Identity
+
+parseT :: ParserT m a -> String -> m [(a, String)]
+parseT (P p) = p
 
 parse :: Parser a -> String -> [(a, String)]
-parse (P p) = p
+parse (P p) = runIdentity . p
 
-item :: Parser Char
+item :: Monad m => ParserT m Char
 item = P $ \case
-    []       -> []
-    (x : xs) -> [(x, xs)]
+    []       -> return []
+    (x : xs) -> return [(x, xs)]
+
+-- Utils
+
+joinMLML :: Monad m => m [m [a]] -> m [a]
+joinMLML mlmlb = do
+    lmlb <- mlmlb
+    if null lmlb then return [] else foldl1 mConcat lmlb
+
+mConcat :: Monad m => m [a] -> m [a] -> m [a]
+mConcat mas mbs = do
+    as <- mas
+    bs <- mbs
+    return $ as ++ bs
 
 -- Sequencing parsers
 
-instance Functor Parser where
+instance Monad m => Functor (ParserT m) where
     -- fmap :: (a -> b) -> Parser a -> Parser b
-    fmap g p = P $ fmap (applyToFst g) . parse p
+    fmap g p = P $ (fmap . fmap) (applyToFst g) . parseT p
         where applyToFst f (a, b) = (f a, b)
 
-instance Applicative Parser where
+instance Monad m => Applicative (ParserT m) where
     -- pure :: a -> Parser a
-    pure v = P $ \inp -> [(v, inp)]
+    pure v = P $ \inp -> return [(v, inp)]
 
     -- <*> :: Parser (a -> b) -> Parser a -> Parser b
-    pg <*> px = P $ concatMap (\(g, out) -> parse (g <$> px) out) . parse pg
+    pg <*> px = P $ \inp ->
+        let mlgout = parseT pg inp
+            mlmlb  = map (\(g, out) -> parseT (g <$> px) out) <$> mlgout
+        in  joinMLML mlmlb
 
-instance Monad Parser where
+instance Monad m => Monad (ParserT m) where
     -- (>>=) :: Parser a -> (a -> Parser b) -> Parser b
-    p >>= f = P $ concatMap (\(v, out) -> parse (f v) out) . parse p
+    p >>= f = P $ \inp ->
+        let mlaout = parseT p inp
+            mlmlb  = map (\(a, out) -> parseT (f a) out) <$> mlaout
+        in  joinMLML mlmlb
 
 -- Making choices
 
-instance Alternative Parser where
+instance Monad m => Alternative (ParserT m) where
     -- empty :: Parser a
-    empty = P $ const []
+    empty = P $ const $ return []
 
     -- (<|>) :: Parser a -> Parser a -> Parser a
-    p <|> q = P $ \inp -> case parse p inp of
-        []  -> parse q inp
-        res -> res
+    p <|> q = P $ \inp -> do
+        res <- parseT p inp
+        case res of
+            []  -> parseT q inp
+            res -> return res
 
 -- Parser concatenation
 (<:>) :: Parser a -> Parser a -> Parser a
-p <:> q = P $ \inp -> parse p inp ++ parse q inp
+p <:> q = P $ \inp -> mConcat (parseT p inp) (parseT q inp)
 
 -- Derived primitives
 
-sat :: (Char -> Bool) -> Parser Char
+sat :: Monad m => (Char -> Bool) -> ParserT m Char
 sat p = do
     x <- item
     if p x then return x else empty
 
-digit :: Parser Char
+digit :: Monad m => ParserT m Char
 digit = sat isDigit
 
-lower :: Parser Char
+lower :: Monad m => ParserT m Char
 lower = sat isLower
 
-upper :: Parser Char
+upper :: Monad m => ParserT m Char
 upper = sat isUpper
 
-letter :: Parser Char
+letter :: Monad m => ParserT m Char
 letter = sat isAlpha
 
-alphanum :: Parser Char
+alphanum :: Monad m => ParserT m Char
 alphanum = sat isAlphaNum
 
-char :: Char -> Parser Char
+char :: Monad m => Char -> ParserT m Char
 char x = sat (== x)
 
-string :: String -> Parser String
+string :: Monad m => String -> ParserT m String
 string []       = return []
 string (x : xs) = do
     char x
     string xs
     return (x : xs)
 
-ident :: Parser String
+ident :: Monad m => ParserT m String
 ident = do
     x  <- lower
     xs <- many alphanum
     return (x : xs)
 
-nat :: Parser Int
+nat :: Monad m => ParserT m Int
 nat = do
     xs <- some digit
     return (read xs)
 
-int :: Parser Int
+int :: Monad m => ParserT m Int
 int =
     do
         char '-'
@@ -117,26 +146,35 @@ int =
 
 -- Handling spacing
 
-space :: Parser ()
+space :: Monad m => ParserT m ()
 space = do
     many (sat isSpace)
     return ()
 
-token :: Parser a -> Parser a
+token :: Monad m => ParserT m a -> ParserT m a
 token p = do
     space
     v <- p
     space
     return v
 
-identifier :: Parser String
+identifier :: Monad m => ParserT m String
 identifier = token ident
 
-natural :: Parser Int
+natural :: Monad m => ParserT m Int
 natural = token nat
 
-integer :: Parser Int
+integer :: Monad m => ParserT m Int
 integer = token int
 
-symbol :: String -> Parser String
+symbol :: Monad m => String -> ParserT m String
 symbol xs = token (string xs)
+
+-- Repeating parsers
+
+repeatP :: Monad m => Integer -> ParserT m a -> ParserT m [a]
+repeatP n p = if n > 0
+    then do
+        a <- p
+        (a :) <$> repeatP (n - 1) p
+    else return []
